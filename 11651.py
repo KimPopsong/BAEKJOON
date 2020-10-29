bf474623d6963c2e04701044c43c8921d4490607
Kim DaeSeong <kimds5344@naver.com>
2020-10-29 15:58:19 +0900

    BAEKJOON

---
+++ 11651.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+location = []
+
+for i in range(n):
+    temp = input()
+    temp = temp.split(' ')
+
+    temp[0] = int(temp[0])
+    temp[1] = int(temp[1])
+
+    location.append(temp)
+
+location.sort()
+
+location = sorted(location, key = lambda x : x[1])
+
+for i in range(n):
+    print('%d %d' %(location[i][0], location[i][1]))
