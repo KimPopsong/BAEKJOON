c7c6f2ae4f0a89c10afd829bba85d5a09adfb9d4
Kim DaeSeong <kimds5344@naver.com>
2020-12-16 19:32:05 +0900

    BAEKJOON

---
+++ 10886.py
@@ -0,0 +1,13 @@
+people = int(input())
+
+cute = 0
+
+for i in range(people):
+    if int(input()):
+        cute += 1
+
+if people // 2 < cute:
+    print('Junhee is cute!')
+
+else:
+    print('Junhee is not cute!')
