c0073c89edba68b9b302f85bd69fd32c4b8e6401
Kim DaeSeong <kimds5344@naver.com>
2021-01-05 17:31:23 +0900

    BAEKJOON

---
+++ 9498.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	if (n >= 90)
+		printf("A\n");
+
+	else if (n >= 80)
+		printf("B\n");
+
+	else if (n >= 70)
+		printf("C\n");
+
+	else if (n >= 60)
+		printf("D\n");
+
+	else
+		printf("F\n");
+
+	return 0;
+}	
