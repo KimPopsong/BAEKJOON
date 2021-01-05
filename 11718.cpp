55f74bdc30755ed0e85323787c0baadd1e95d2e8
Kim DaeSeong <kimds5344@naver.com>
2021-01-05 17:33:12 +0900

    BAEKJOON

---
+++ 11718.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a;
+ 
+    while (1) {
+    
+        a = getchar();
+ 
+        if (a == EOF) {
+        
+            
+            break;
+        }
+ 
+        putchar(a);
+ 
+    }
+ 
+    return 0;
+}
