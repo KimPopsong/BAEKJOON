1110f4bed0d5bdada973f7209668001cccb20b14
Kim DaeSeong <kimds5344@naver.com>
2021-01-15 23:54:59 +0900

    BAEKJOON

---
+++ 15829.py
@@ -0,0 +1,10 @@
+n = int(input())
+alphabet = [i for i in range(27)]
+sumHash = 0
+
+string = input()
+
+for i in range(n):
+    sumHash += (alphabet[ord(string[i]) - 96]) * (31 ** i)
+
+print(sumHash)
