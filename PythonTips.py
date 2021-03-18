4f285edd021756e338c75b1db42ceb5d9211e587
Kim DaeSeong <kimds5344@naver.com>
2021-03-18 14:58:51 +0900

    2021. 03. 18.

---
+++ PythonTips.py
@@ -0,0 +1,55 @@
+#Eratosthenes
+import math
+
+eratosthenes = []
+maximum = int(input())
+for i in range(0, maximum + 1):
+    eratosthenes.append(True)
+
+    if i % 2 == 0:
+        eratosthenes[i] = False
+
+eratosthenes[0] = False
+eratosthenes[1] = False
+eratosthenes[2] = True
+
+i = 1
+while True:
+    i += 1
+
+    if i % 2 == 0:
+        continue
+
+    if i > math.sqrt(maximum) + 1:
+        break
+
+    k = 2
+    while i * k <= maximum:
+        eratosthenes[k * i] = False
+        k += 1
+
+print(eratosthenes)
+
+#Sorted List
+list1 = [[1, 2], [3, 4], [5, 6]]
+
+sorted_list = sorted(list1, key=lambda x: x[0], reverse=True)
+
+print(sorted_list)
+
+#Stdin
+import sys
+
+a = sys.stdin.readline()
+x, y = sys.stdin.readline().rsplit()
+l = list(map(int, sys.stdin.readline().rsplit()))
+c = int(sys.stdin.readline().rstrip())  # rstrip not rsplit
+
+#Heap Queue
+import heapq
+
+list = []
+data = int(input())
+
+heapq.heappush(list, data)
+n = heapq.heappop(list)
