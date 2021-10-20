8c9ec3a4dfbf127cff01857e880ddb28d6c7f395
Kim DaeSeong <kimds5344@naver.com>
2021-10-20 17:42:31 +0900

    2021. 10. 20.

---
+++ TipsCPP.cpp
@@ -0,0 +1,16 @@
+///////////////////////////////
+// Fast Input Output
+#include <iostream>
+
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  std::cout << "\n";  // Faster than std::endl
+  
+  return 0;
+}
+
+///////////////////////////////
