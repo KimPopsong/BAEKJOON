4651a7b9452b7abaa81f0146d69476c232901f8e
Kim DaeSeong <kimds5344@naver.com>
2021-10-21 19:41:40 +0900

    2021. 10. 21.

---
+++ 14645.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	int stationNumber, peopleNum;
+
+	for (int i = 0; i < stationNumber; i++)
+	{
+		int up, down;
+
+		std::cin >> up >> down;
+	}
+
+	std::cout << "비와이" << std::endl;
+
+	return 0;
+}
