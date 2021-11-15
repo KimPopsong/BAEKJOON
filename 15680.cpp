f331e243b4ae652b4e2d3789a20656e5904b4283
Kim DaeSeong <kimds5344@naver.com>
2021-11-15 12:18:46 +0900

    2021. 11. 15.

---
+++ 15680.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	int nod;
+
+	std::cin >> nod;
+
+	if (nod)
+	{
+		std::cout << "Leading the Way to the Future";
+	}
+
+	else
+	{
+		std::cout << "YONSEI";
+	}
+
+	std::cout << "\n";
+
+	return 0;
+}
