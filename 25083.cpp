1d963dcdc983c38fb8263772ff659ca439be076e
Kim DaeSeong <kimds5344@naver.com>
2023-11-04 22:38:20 +0900

    2023. 11. 04.

---
+++ 25083.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "         ,r'\"7" << std::endl;
+	std::cout << "r`-_   ,'  ,/" << std::endl;
+	std::cout << " \\. \". L_r'" << std::endl;
+	std::cout << "   `~\\/" << std::endl;
+	std::cout << "      |" << std::endl;
+	std::cout << "      |" << std::endl;
+
+	return 0;
+}
