df57e98795dfec2456ff336610f66fe5186cb50e
Kim DaeSeong <kimds5344@naver.com>
2025-02-11 14:39:14 +0900

    Create 1940.java

---
+++ 1940.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		int materialNumber = Integer.parseInt(br.readLine());
+		int armorNeed = Integer.parseInt(br.readLine());
+		int count = 0;
+		int[] materials = new int[materialNumber];
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < materialNumber; i++) {
+			materials[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < materialNumber - 1; i++) {
+			int materialA = materials[i];
+
+			for (int j = i + 1; j < materialNumber; j++) {
+				if (materialA + materials[j] == armorNeed) {
+					count += 1;
+				}
+			}
+		}
+
+		System.out.println(count);
+	}
+}
