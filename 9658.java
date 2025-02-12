7e48c52503d14812260883803678b656c2ca5ce0
Kim DaeSeong <kimds5344@naver.com>
2025-02-12 12:28:37 +0900

    2025. 02. 12.

---
+++ 9658.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		long stone = Long.parseLong(br.readLine());
+
+		int left = (int) (stone % 7);
+
+		if (left == 1 || left == 3) {
+			System.out.println("CY");
+		}
+
+		else {
+			System.out.println("SK");
+		}
+	}
+}
