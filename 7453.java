fe21d4d367d76d701bedceeb59ebf9a9ce4b715f
Kim DaeSeong <kimds5344@naver.com>
2025-02-17 20:10:49 +0900

    2025. 02. 17.

--- 7453.java
+++ 7453.java
@@ -1,6 +1,7 @@
-import java.io.*;
-import java.util.*;
-import java.util.Map.Entry;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String args[]) throws Exception
