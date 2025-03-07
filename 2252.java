7598c9bdbef2ae5ba537cadee2f0ba02ee6fb275
Kim DaeSeong <kimds5344@naver.com>
2025-03-07 11:43:56 +0900

    2025. 03. 07.

--- 2252.java
+++ 2252.java
@@ -1,5 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
