4b13b87b87b75d922851ab2a7bde0e9094df6d84
Kim DaeSeong <kimds5344@naver.com>
2025-03-07 11:43:25 +0900

    2025. 03. 05.

---
+++ 2252.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+
+		int studentNumber, compareNumber;
+
+		st = new StringTokenizer(br.readLine());
+		studentNumber = Integer.parseInt(st.nextToken());
+		compareNumber = Integer.parseInt(st.nextToken());
+
+		ArrayList<Integer>[] compare = new ArrayList[studentNumber + 1]; // [ 학생 : [ 학생의 앞에 있어야 하는 사람들의 목록 ] ]
+		int[] degree = new int[studentNumber + 1]; // 아래 있는
+
+		for (int i = 1; i <= studentNumber; i++) {
+			compare[i] = new ArrayList<>();
+		}
+
+		for (int i = 0; i < compareNumber; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int studentSmall = Integer.parseInt(st.nextToken());
+			int studentBig = Integer.parseInt(st.nextToken());
+
+			compare[studentBig].add(studentSmall);
+			degree[studentSmall] += 1;
+		}
+
+		ArrayDeque<Integer> topology = new ArrayDeque<>();
+		for (int i = 1; i <= studentNumber; i++) {
+			if (degree[i] == 0) {
+				topology.add(i);
+			}
+		}
+
+		ArrayDeque<Integer> answer = new ArrayDeque<>();
+
+		while (!topology.isEmpty()) {
+			int student = topology.removeFirst();
+
+			answer.addFirst(student);
+
+			for (int i : compare[student]) {
+				degree[i] -= 1;
+
+				if (degree[i] == 0) {
+					topology.addLast(i);
+				}
+			}
+		}
+
+		while (!answer.isEmpty()) {
+			sb.append(answer.removeFirst()).append(" ");
+		}
+
+		System.out.println(sb);
+	}
+}
