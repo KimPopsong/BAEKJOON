f5bc04b1ce15278522199a378f6b0df411374c99
Kim DaeSeong <kimds5344@naver.com>
2025-06-26 13:46:37 +0900

    2025. 06. 26.

--- 1158.java
+++ 1158.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = null;
         StringBuilder sb = new StringBuilder();
 
         st = new StringTokenizer(br.readLine());
