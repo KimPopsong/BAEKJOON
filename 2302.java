eb1dba92af245af8068acd5170212b618ff628cb
Kim DaeSeong <kimds5344@naver.com>
2025-10-27 12:53:03 +0900

    2025. 10. 27.

--- 2302.java
+++ 2302.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     static int seatNumber, vipNumber, sumAvail = 1;
